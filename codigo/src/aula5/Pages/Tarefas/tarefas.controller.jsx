23fc79ef986179794417cb06d533d7b6869ab9ea
Ricardo Glodzinski - DATAPREVSC <ricardo.glodzinski@dataprev.gov.br>
2020-08-16 17:45:16 -0300

    Aula5

--- codigo/src/aula5/Pages/Tarefas/tarefas.controller.jsx
+++ codigo/src/aula5/Pages/Tarefas/tarefas.controller.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import ListaTarefas from "./tarefas.list";
 
-const TAREFAS_API_BASE_URL = "http://localhost:3000";
+const TAREFAS_API_BASE_URL = "http://10.107.5.150/api";
 const jsonHeaders = new Headers({ "Content-Type": "application/json" });
 
 const Tarefas = () => {
